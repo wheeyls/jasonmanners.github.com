da10e54e8536e531e1b928527a0ed853ec64781e
jasonmanners <jasonmanners@gmail.com>
2011-05-25 21:12:11 +0000

    added block

--- js/pacMonster.js
+++ js/pacMonster.js
@@ -13,16 +13,17 @@ var startTime = window.mozAnimationStartTime || Date.now();
 function Block(x,y) {
   this.x = x;
   this.y = y;
-  this.canvas = $("<canvas/>").attr("width", 200).attr("height", 200).get(0);
+  this.canvas = $("<canvas/>").attr("width", 100).attr("height", 100).get(0);
   this.context = this.canvas.getContext("2d");
 }
 
 Block.prototype.init_canvas = function() {
   this.context.save();
-    this.context.fillStyle = "#FFEE00";
-    this.context.beginPath();
-    this.context.arc(100, 100, 50, 0, Math.PI*2, true);
-    this.context.fill();
+    this.context.fillStyle = "rgb(130,130,255)";
+    this.context.fillRect (0, 0, 20, 20);
+    this.context.fillRect (20, 0, 20, 20);
+    this.context.fillRect (20, 20, 20, 20);
+    this.context.fillRect (20, 40, 20, 20);
   this.context.restore();
 };
 
@@ -48,12 +49,14 @@ function World() {
   this.WORLD_HEIGHT = 300;
   this.context = null;
   this.pacMonster = new PacMonster(50,100);
+  this.block = new Block(300,200);
 }
 
 World.prototype.init_world = function() {
   $("#world").attr({ width: this.WORLD_WIDTH, height: this.WORLD_HEIGHT });
   this.context = $('#world')[0].getContext("2d");
   this.pacMonster.init_canvas();
+  this.block.init_canvas();
 };
 
 World.prototype.draw = function(context) {
@@ -61,6 +64,7 @@ World.prototype.draw = function(context) {
   context.fillStyle = "rgb(255,255,255)";
   context.fillRect (0, 0, this.WORLD_WIDTH, this.WORLD_HEIGHT);
   context.drawImage(this.pacMonster.canvas,this.pacMonster.x,this.pacMonster.y);
+  context.drawImage(this.block.canvas,this.block.x,this.block.y);
 };
 
 World.prototype.run = function(timestamp) {
