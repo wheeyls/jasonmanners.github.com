569262ae3a3e8aa77bdb8f90340053c650b40825
jasonmanners <jasonmanners@gmail.com>
2011-05-26 03:27:46 +0000

    first pass

--- js/pacMonster.js
+++ js/pacMonster.js
@@ -13,6 +13,8 @@ var startTime = window.mozAnimationStartTime || Date.now();
 function Block(x,y) {
   this.x = x;
   this.y = y;
+  this.width = 10;
+  this.height = 10;
   this.canvas = $("<canvas/>").attr("width", 100).attr("height", 100).get(0);
   this.context = this.canvas.getContext("2d");
 }
@@ -20,16 +22,17 @@ function Block(x,y) {
 Block.prototype.init_canvas = function() {
   this.context.save();
     this.context.fillStyle = "rgba(75,130,255,0.5)";
-    this.context.fillRect (0, 40, 20, 20);
-    this.context.fillRect (20, 0, 20, 20);
-    this.context.fillRect (20, 20, 20, 20);
-    this.context.fillRect (20, 40, 20, 20);
+    this.context.fillRect (0, 20, this.width, this.height);
+    this.context.fillRect (10, 0, this.width, this.height);
+    this.context.fillRect (10, 10, this.width, this.height);
+    this.context.fillRect (10, 20, this.width, this.height);
   this.context.restore();
 };
 
 function PacMonster(x,y) {
   this.x = x;
   this.y = y;
+  this.radius = 50;
   this.canvas = $("<canvas/>").attr("width", 200).attr("height", 200).get(0);
   this.context = this.canvas.getContext("2d");
   this.canvas2 = $("<canvas/>").attr("width", 200).attr("height", 200).get(0);
@@ -41,7 +44,7 @@ PacMonster.prototype.init_canvas = function() {
   this.context.save();
     this.context.fillStyle = "rgba(255,238,0,1.0)";
     this.context.beginPath();
-    this.context.arc(100, 100, 100, Math.PI*1.80, Math.PI*0.2, true);
+    this.context.arc(100, 100, this.radius, Math.PI*1.80, Math.PI*0.2, true);
     this.context.lineTo(100, 100);
     this.context.closePath();
     this.context.fill();
@@ -50,7 +53,7 @@ PacMonster.prototype.init_canvas = function() {
   this.context2.save();
     this.context2.fillStyle = "rgba(255,238,0,1.0)";
     this.context2.beginPath();
-    this.context2.arc(100, 100, 100, 0, Math.PI*2, true);
+    this.context2.arc(100, 100, this.radius, 0, Math.PI*2, true);
     this.context2.fill();
   this.context2.restore();
 
@@ -79,7 +82,7 @@ World.prototype.draw = function(context) {
   context.fillStyle = "rgba(255,255,255,0)";
   context.fillRect (0, 0, this.WORLD_WIDTH, this.WORLD_HEIGHT);
   //Convert to own canvas for bg image
-    context.save();
+  /*  context.save();
   // Create radial gradient
   grad = context.createRadialGradient(300,300,0,300,300,325); 
   grad.addColorStop(0, "rgba(0,0,150,0.25)");
@@ -87,11 +90,12 @@ World.prototype.draw = function(context) {
  
   // assign gradients to fill
   context.fillStyle = grad;
- 
+
   // draw 600x600 fill
   context.fillRect(0,0,this.WORLD_WIDTH,this.WORLD_HEIGHT);
+   */
   context.save();
-    context.scale(0.25,0.25);
+    //context.scale(0.25,0.25);
     context.translate(-this.camera.x,this.camera.y);
     context.drawImage(this.block.canvas,this.block.x,this.block.y);
     context.drawImage(this.pacMonster.canvases[Math.floor(this.i/10)],this.pacMonster.x,this.pacMonster.y);
