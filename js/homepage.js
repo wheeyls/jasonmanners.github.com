e934341edad1d472f658a5bda6f36bbf8f558280
jasonmanners <jasonmanners@gmail.com>
2011-05-27 04:10:57 +0000

    testing

--- js/homepage.js
+++ js/homepage.js
@@ -16,19 +16,32 @@ function FractalPoint(x,y) {
   this.x = x;
   this.y = y;
   this.lifespan = 0;
+  this.color = "rgba("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+",0.8)";
 }
 
 FractalPoint.prototype.update = function(delat_time) {
 };
 
 FractalPoint.prototype.draw = function(context) {
-  context.save();
+  /*context.save();
     context.beginPath();
-    context.fillColor = "rgba("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+",0.8)";
+    context.fillStyle = this.color;
+    context.moveTo(this.x, this.y);
     context.arc(this.x, this.y, 30, 0, Math.PI * 2, true);
     context.closePath();
     context.fill();
   context.restore();
+  */
+  context.save();
+    context.fillStyle = this.color;
+    context.shadowColor = this.color;
+    context.shadowBlur = 3;
+    context.beginPath();
+    context.moveTo(this.x, this.y);
+    context.arc(this.x, this.y, 5, 0, 2 * Math.PI, true);
+    context.closePath();
+    context.fill();
+    context.restore();
 };
 
 /************************************
@@ -41,7 +54,7 @@ function World() {
   this.context = null;
   this.treePoints = [{x : Math.floor(Math.random()*10), y : Math.floor(Math.random()*10), frag: 0.1}];
   this.timePassed = 0;
-  this.fractalPoint = new FractalPoint(100,100);//this.WORLD_WIDTH/2,this.WORLD_HEIGHT/2);
+  this.fractalPoint = new FractalPoint(this.WORLD_WIDTH/2,this.WORLD_HEIGHT/2);
 }
 
 World.prototype.init_world = function() {
@@ -50,28 +63,10 @@ World.prototype.init_world = function() {
 };
 
 World.prototype.draw = function(context) {
-  context.clearRect(0, 0, this.WORLD_WIDTH, this.WORLD_HEIGHT);
-  context.fillStyle = "rgba(255,255,250,0.5)";
-  context.fillRect (0, 0, this.WORLD_WIDTH, this.WORLD_HEIGHT);
-  context.save();
-    context.lineWidth = 2;
-    context.beginPath();
-    context.moveTo(0,0);
-    var x = 0;
-    var y = 0;
-    for(var i = 0; i < this.treePoints.length; i++) {
-      x += (this.treePoints[i].x * this.treePoints[i].frag);
-      y += (this.treePoints[i].y * this.treePoints[i].frag);
-      context.lineTo(x, y);
-    }
-    
-    context.strokeStyle = "rgba(0,0,0,0.5)";
-    context.shadowColor = "rgba(200,130,255,0.8)";
-    context.shadowOffsetX = 2;
-    context.shadowOffsetY = 2;
-    context.shadowBlur = 20;
-    context.stroke();
-  context.restore();
+  //Don't want to clear because it will cause the drawings to clear
+  //context.clearRect(0, 0, this.WORLD_WIDTH, this.WORLD_HEIGHT);
+  //context.fillStyle = "rgba(255,255,250,0.5)";
+  //context.fillRect (0, 0, this.WORLD_WIDTH, this.WORLD_HEIGHT);
   this.fractalPoint.draw(context);
 };
 
@@ -82,6 +77,8 @@ World.prototype.update = function(delta_time) {
     this.treePoints.push({x : Math.floor(Math.random()*10), y : Math.floor(Math.random()*10), frag: 0.05});
     this.timePassed = 0;
   }
+  this.fractalPoint.x += Math.random() * 5 - 2.5;
+  this.fractalPoint.y -= Math.random() * 5;
 };
 
 World.prototype.run = function(timestamp) {
