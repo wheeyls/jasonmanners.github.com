9a77f796fdd785e2dd71c95b1befbe1939e191f4
jasonmanners <jasonmanners@gmail.com>
2011-05-27 16:09:17 +0000

    Fixed requestAnimationFrame

--- js/sun.js
+++ js/sun.js
@@ -2,11 +2,10 @@
   RequestAnimationFrame declaration
 *************************************/
 var requestAnimationFrame =   window.mozRequestAnimationFrame     || 
-                              window.webkitRequestAnimationFrame  || 
-                              function(callback){
-                                window.setTimeout(callback, MS_IN_SEC / 60);
+                              window.webkitRequestAnimationFrame  ||
+                              function(/* function */ callback, /* DOMElement */ element){
+                                 window.setTimeout(callback, 1000 / 60, +new Date());
                               };
-                            
 var startTime = window.mozAnimationStartTime || Date.now();
 
 /************************************
