ae79cfd1eaa4048d37c8c00f88c9aa29d7463f61
jasonmanners <jasonmanners@gmail.com>
2011-05-28 03:08:48 +0000

    trying setTimeout

--- js/sun.js
+++ js/sun.js
@@ -4,7 +4,7 @@
 var requestAnimationFrame =   window.mozRequestAnimationFrame     || 
                               window.webkitRequestAnimationFrame  ||
                               function(/* function */ callback, /* DOMElement */ element){
-                                 window.setTimeout(callback(), 1000 / 60, +new Date());
+                                 window.setTimeout("callback()", 1000 / 60, +new Date());
                               };
 var startTime = window.mozAnimationStartTime || Date.now();
 
