df5f8aeb9e15d6cf323b9d97e0c95752530d88ac
jasonmanners <jasonmanners@gmail.com>
2011-05-28 04:16:59 +0000

    Starting engine

---
+++ skittles/js/engine.js
@@ -0,0 +1,9 @@
+/************************************
+  RequestAnimationFrame declaration
+*************************************/
+var requestAnimationFrame =   window.mozRequestAnimationFrame     || 
+                              window.webkitRequestAnimationFrame  ||
+                              function(/* function */ callback, /* DOMElement */ element){
+                                 window.setTimeout(callback, 1000 / 60, new Date());
+                              };
+var startTime = window.mozAnimationStartTime || Date.now();
