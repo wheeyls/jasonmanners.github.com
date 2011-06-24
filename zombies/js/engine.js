4dc8831e5626b5321a131e1f5efbb7641835320f
jasonmanners <jasonmanners@gmail.com>
2011-06-24 21:39:08 +0000

    fixing requestAnimFrame

--- zombies/js/engine.js
+++ zombies/js/engine.js
@@ -22,7 +22,7 @@ window.requestAnimFrame = (function(){
               window.msRequestAnimationFrame     || 
               function(/* function */ callback, /* DOMElement */ element){
                 window.setTimeout(callback, 1000 / 60, new Date());
-              };
+              }();
     })();
 var startTime = window.mozAnimationStartTime || Date.now();
 
